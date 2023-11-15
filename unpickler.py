5193285c3a0a8e479cb312b9b07d695a9c61b937
Johnny <jmnaze123@gmail.com>
2023-11-15 01:28:38 -0500

    Update unpickler.py
    - soft coded

--- unpickler.py
+++ unpickler.py
@@ -1,6 +1,12 @@
 import pickle
 import os
 
+MILESTONE2_PATH = "Milestone2/"
+MILESTONE3_PATH = "Milestone3/"
+GENERATED_SUBTITLE_FILE_NAME = "generated_subtitles.pickle"
+TRANSLATED_SUBTITLE_FILE_NAME = "translated_subtitles.pickle"
+
+
 if __name__ == "__main__":
     print(
         "What would you like to unpickle?\n[1] Generated subtitles\n[2] Translated subtitles"
@@ -9,19 +15,19 @@ if __name__ == "__main__":
     if choice == "1":
         print("What video ID would you like to unpickle?")
         count = 0
-        for video_id in os.listdir("Milestone2/"):
+        for video_id in os.listdir(MILESTONE2_PATH):
             count += 1
             print(f"[{count}] {video_id}")
 
         choice = input()
-        if int(choice) > len(os.listdir("Milestone2/")):
+        if int(choice) > len(os.listdir(MILESTONE2_PATH)):
             print("Invalid choice")
             exit(1)
 
-        video_id = os.listdir("Milestone2/")[int(choice) - 1]
+        video_id = os.listdir(MILESTONE2_PATH)[int(choice) - 1]
 
         with open(
-            f"Milestone2/{video_id}/generated_subtitles.pickle", "rb"
+            f"{MILESTONE2_PATH}{video_id}/{GENERATED_SUBTITLE_FILE_NAME}", "rb"
         ) as generated_subtitles_file:
             generated_subtitles = pickle.load(generated_subtitles_file)
 
@@ -30,32 +36,33 @@ if __name__ == "__main__":
     elif choice == "2":
         print("What video ID would you like to unpickle?")
         count = 0
-        for video_id in os.listdir("Milestone3/"):
+        for video_id in os.listdir(MILESTONE3_PATH):
             count += 1
             print(f"[{count}] {video_id}")
 
         choice = input()
-        if int(choice) > len(os.listdir("Milestone3/")):
+        if int(choice) > len(os.listdir(MILESTONE3_PATH)):
             print("Invalid choice")
             exit(1)
 
-        video_id = os.listdir("Milestone3/")[int(choice) - 1]
+        video_id = os.listdir(MILESTONE3_PATH)[int(choice) - 1]
 
         print("What language would you like to unpickle?")
         count = 0
-        for language in os.listdir(f"Milestone3/{video_id}/"):
+        for language in os.listdir(f"{MILESTONE3_PATH}{video_id}/"):
             count += 1
             print(f"[{count}] {language}")
 
         choice = input()
-        if int(choice) > len(os.listdir(f"Milestone3/{video_id}/")):
+        if int(choice) > len(os.listdir(f"{MILESTONE3_PATH}{video_id}/")):
             print("Invalid choice")
             exit(1)
 
-        language = os.listdir(f"Milestone3/{video_id}/")[int(choice) - 1]
+        language = os.listdir(f"{MILESTONE3_PATH}{video_id}/")[int(choice) - 1]
 
         with open(
-            f"Milestone3/{video_id}/{language}/translated_subtitles.pickle", "rb"
+            f"{MILESTONE3_PATH}{video_id}/{language}/{TRANSLATED_SUBTITLE_FILE_NAME}",
+            "rb",
         ) as translated_subtitles_file:
             translated_subtitles = pickle.load(translated_subtitles_file)
 
